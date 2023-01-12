992f602e8d2a4a80199e0339145424469b7d53ac
Alan Zhong <azh248@gmail.com>
2023-01-11 23:31:01 -0500

    more content

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -3,12 +3,14 @@ import { AppConfig } from '../utils/AppConfig';
 import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
+import { MiddleVerticalFeatures } from './MiddleVerticalFeatures';
 import { VerticalFeatures } from './VerticalFeatures';
 
 const Base = () => (
   <div className="antialiased text-gray-600">
     <Meta title={AppConfig.title} description={AppConfig.description} />
     <Hero />
+    <MiddleVerticalFeatures />
     <VerticalFeatures />
     <Banner />
     <Footer />
--- src/templates/MiddleVerticalFeatures.tsx
+++ src/templates/MiddleVerticalFeatures.tsx
@@ -1,31 +1,25 @@
 import { VerticalFeatureRow } from '../feature/VerticalFeatureRow';
 import { Section } from '../layout/Section';
 
-const VerticalFeatures = () => (
+const MiddleVerticalFeatures = () => (
   <Section
-    title="See How We Streamlined Malaria Screening"
-    description="Hint: by breaking down the expensive and unnecessary diagnosis process and replacing it with cheaper alternatives!"
+    title="So What Is Malaria Screening?"
+    description="And why is it important?"
   >
     <VerticalFeatureRow
-      title="Using 3D-printing to power up smartphones."
-      description="Traditional malaria diagnosis has always relied on expensive microscopes to see blood cells up close. We've replaced that with affordable, 3D-printed smartphone attachments to achieve 200x magnification at a widely accessible price point."
+      title="title."
+      description="description."
       image="/assets/images/feature.svg"
-      imageAlt="Smartphone attachment"
+      imageAlt=""
     />
     <VerticalFeatureRow
-      title="Deep learning-based diagnosis"
-      description="text here."
+      title="title."
+      description="description."
       image="/assets/images/feature2.svg"
       imageAlt="Deep learning page"
       reverse
     />
-    <VerticalFeatureRow
-      title="Your title here"
-      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, nunc non posuere consectetur, justo erat semper enim, non hendrerit dui odio id enim."
-      image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
-    />
   </Section>
 );
 
-export { VerticalFeatures };
+export { MiddleVerticalFeatures };
